ddb2a1b6dd11f5d20bb1ca50c0d0fb53296f68a8
nicolas.hanteville@gmail.com <nicolas.hanteville@gmail.com@5af39df0-c4b3-bcfc-c6e6-7e8ae1137c0f>
2013-07-31 13:06:57 +0000

    [31/07/2013] Version 0.6.0.0a Adding functionalities:   - review bug for IE in 64b OS   - review few bug   - x64 binary update

--- RTCA/version.h
+++ RTCA/version.h
@@ -8,17 +8,17 @@
 	//Standard Version Type
 	static const long MAJOR = 0;
 	static const long MINOR = 6;
-	static const long BUILD = 753;
-	static const long REVISION = 89;
+	static const long BUILD = 1;
+	static const long REVISION = 6;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 7478;
-	#define RC_FILEVERSION 0,6,753,89
-	#define RC_FILEVERSION_STRING "0, 6, 753, 89\0"
-	static const char FULLVERSION_STRING[] = "0.6.753.89";
+	static const long BUILDS_COUNT = 0;
+	#define RC_FILEVERSION 0,6,1,6
+	#define RC_FILEVERSION_STRING "0, 6, 1, 6\0"
+	static const char FULLVERSION_STRING[] = "0.6.1.6";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY = 219;
+	static const long BUILD_HISTORY = 221;
 	
 
 #endif //VERSION_H
